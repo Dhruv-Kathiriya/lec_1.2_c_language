4545be3d046dab689ab00962f2657066b3d47b64
dhruv <dhruvkathiriya933@gmail.com>
2023-11-06 18:31:45 +0530

    first lec...

---
+++ TASK1 (1).C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  clrscr();
+  printf("My name is dhruv\n");
+  printf("I am 18 yeras old\n");
+  printf("My last school name is bhakti international school\n");
+  printf("My cours is flutter\n");
+  printf("My GRID 4335\n");
+  printf("85,shiv shakti bangloes,pasodara,kamrej\n");
+  getch();
+}
